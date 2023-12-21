07cafa00c9c62b75f063d7093b38216264259490
Lattae <105736002+Danny2Forever@users.noreply.github.com>
2023-12-21 22:10:47 +0700

    Create height.java

---
+++ lab03/height.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication1;
+import java.util.*;
+/**
+ *
+ * @author User
+ */
+public class height {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter height : ");
+        int height = sc.nextInt();
+        for (int i=0; i<height; i++){
+            System.out.println("#-#-#-#-#");
+        }
+    }
+}
