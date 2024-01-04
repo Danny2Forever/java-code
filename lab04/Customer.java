8c82bb76a8cad7b146a6e8bbc3f8194dbcf8c088
Lattae <105736002+Danny2Forever@users.noreply.github.com>
2024-01-04 16:10:26 +0700

    Create Customer.java

---
+++ lab04/Customer.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author User
+ */
+public class Customer {
+    public String name;
+    public MyDate DOB;
+    public Account acct;
+    
+}
